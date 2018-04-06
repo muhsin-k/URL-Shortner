60945acbea20f0b78b5d7c51eb44572afcd839a6
MUHSIN K <muhzi4u@gmail.com>
2018-04-06 22:00:25 +0530

    bug fix

--- client/src/config/constants.js
+++ client/src/config/constants.js
@@ -1,4 +1,6 @@
 module.exports = {
   apiUrl: "http://localhost:7000/api/",
   baseUrl: "http://localhost"
+  //  /apiUrl: "http://shortener.muhzi.com/v1/api/",
+  // baseUrl: "http://muhzi.com"
 };
